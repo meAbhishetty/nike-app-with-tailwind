f91afb2d046b688fe0a7c3b2abae01fae2eb0b55
Abhishek Shetty <abhishekshetty817@gmail.com>
2023-10-11 23:37:26 +0530

    completed hero section

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
-import {headerLogo} from '../assets/images'
-import {hamburger} from '../assets/icons'
+import React from "react";
+import { headerLogo } from "../assets/images";
+import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants/index.js";
 
 const Nav = () => {
-  return (
-    <header>
-        <nav>
-            <a href='/'>
-                <img src={headerLogo}/>
-            </a>
-        </nav>
-    </header>
-  )
-}
+    return (
+        <header className="padding-x py-8 absolute z-10 w-full">
+            <nav className="flex items-center justify-between max-container">
+                <a href="/">
+                    <img src={headerLogo} width={130} height={29} alt="Logo" />
+                </a>
+                <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+                    {navLinks.map((navLink) => (
+                        <li key={navLink?.label}>
+                            <a
+                                href={navLink?.href}
+                                className="font-montserrat leading-normal text-lg text-slate-gray"
+                            >
+                                {navLink?.label}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+                <div className="lg:hidden">
+                  <img src={hamburger} alt="Hamburger" width={25} height={25}/>
+                </div>
+            </nav>
+        </header>
+    );
+};
 
 export default Nav;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons";
+import { shoes, statistics } from "../constants";
+import { bigShoe1 } from "../assets/images";
+import ShoeCard from "../components/ShoeCard";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+    const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+    return (
+        <section className="w-full min-h-screen flex flex-col xl:flex-row max-container justify-center gap-10">
+            <div className="relative xl:w-2/5 flex flex-col w-full justify-center items-start max-xl:padding-x pt-28">
+                <p className="text-xl text-coral-red font-montserrat">
+                    Our Summer Collection
+                </p>
+                <h1 className="text-8xl font-palanquin mt-10 max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+                    <span className="xl:bg-white xl:whitespace-nowrap z-10 relative pr-10">
+                        The New Arrival
+                    </span>
+                    <br />
+                    <span className="text-coral-red mt-3 inline-block">
+                        Nike
+                    </span>{" "}
+                    <span>Shoes</span>
+                </h1>
+                <p className="font-montserrat mt-6 mb-14 sm:max-w-sm text-lg leading-8 text-slate-gray">
+                    Discover stylish Nike arrivals, quality comfort and
+                    innovation for your active life.
+                </p>
+                <Button label="Shop now" iconUrl={arrowRight} />
+                <div className="flex justify-start items-start flex-wrap mt-20 w-full gap-16">
+                    {statistics?.map((stat, index) => (
+                        <div key={index}>
+                            <p className="font-palanquin text-4xl font-bold">
+                                {stat?.value}
+                            </p>
+                            <p className="font-montserrat leading-7 text-slate-gray">
+                                {stat?.label}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-cover bg-hero bg-center">
+                <img
+                    src={bigShoeImg}
+                    alt="Shoe Collection"
+                    width={610}
+                    height={500}
+                    className="z-10 object-contain relative"
+                />
+                <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+                    {shoes?.map((shoe) => (
+                        <div key={shoe.thumbnail}>
+                            <ShoeCard
+                                imgURL={shoe}
+                                changeBigImg={setBigShoeImg}
+                                bigShoeImg={bigShoeImg}
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+};
 
 export default Hero;
